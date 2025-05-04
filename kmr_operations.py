e97dfdfc2184b86b842e05710cfb29e92349146a
sergson <serg_terihov@mail.ru>
2025-05-04 18:31:51 +0700

    layout errors corrected

--- kmr_operations.py
+++ kmr_operations.py
@@ -9,14 +9,14 @@ __author__ = "Sergei Terikhov"
 KMR Operator Algebra v1.0
 
 Defines direct (⊙) and inverse (⊘) KMR transformations:
-- A ⊙ B = A/(1 + AB)
-- A ⊘ B = A/(1 - AB)
+- A ⊙ K = A/(1 + AK)
+- A ⊘ K = A/(1 - AK)
 """
 
-def kmr_direct(a, b):
-    """A ⊙ B = A/(1 + AB)"""
-    return a / (1 + a*b)
+def kmr_direct(A, K):
+    """A ⊙ K = A/(1 + AK)"""
+    return A / (1 + A*K)
 
-def kmr_inverse(a, b):
-    """A ⊘ B = A/(1 - AB)"""
-    return a / (1 - a*b)
+def kmr_inverse(A, K):
+    """A ⊘ K = A/(1 - AK)"""
+    return A / (1 - A*K)
